ceebaed3468721dcae52570c7a0e14ccb4459a29
pameladilly <pameladilly@hotmail.com>
2021-04-04 19:39:23 -0300

    config yml

--- src/test/java/com/pameladilly/resources/AddNewUserTest.java
+++ src/test/java/com/pameladilly/resources/AddNewUserTest.java
@@ -26,7 +26,7 @@ public class AddNewUserTest {
 
   @BeforeAll
   public static void setUp() {
-
+    System.setProperty("webdriver.chrome.driver", "/usr/local/share/chromedriver");
     AddNewUserTest.driver = new ChromeDriver();
 
     AddNewUserTest.context = SpringApplication.run(Application.class);
